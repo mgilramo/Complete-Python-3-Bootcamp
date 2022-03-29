d543f464e600d498d970da6ee536739634f0f4cc
Manuel Gil Ramos <mgilramo@emeal.nttdata.com>
2022-03-29 16:11:40 +0200

    Initial Commit - Personal Folder (99-Manuel Folder)

---
+++ 99-Manuel Folder/Modules and Packages/myprogram.py
@@ -0,0 +1,7 @@
+from mymodule import my_func
+from MyMainPackage import some_main_script as main_s
+from MyMainPackage.SubPackage import mysubscript as sub_s
+
+my_func()
+main_s.report_main()
+sub_s.sub_report()
